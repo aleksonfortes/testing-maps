1908314d1506de55f49464129726a97e991df592
Alekson Fortes <alekson.fortes@gmail.com>
2026-03-21 13:24:18 -0400

    CI: Refactor pipeline for parallelization, caching, and production e2e testing

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: 'npm run dev',
+    command: process.env.CI ? 'npm run start' : 'npm run dev',
     url: process.env.BASE_URL || 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
     env: {
